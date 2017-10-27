51540f579c577b8592c41e04f961bd13e2d78785
Mykah <mykah.taylor@gmail.com>
2017-10-27 17:37:38 -0400

    Setting up Gulp task

---
+++ product-listing/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+  gulp.src('./stylus/style.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/'))
+    .pipe(browserSync.reload({
+      stream:true
+    }))
+})
+gulp.task('views', () => {
+  gulp.src('./views/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'))
+})
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  })
+})
+gulp.task('watch', ['browser-sync', 'styles', 'views'], () => {
+  gulp.watch('./stylus/*.styl', ['styles'])
+  gulp.watch("*.html").on("change", reload) 
+  // gulp.watch(['./script/src/*.js'], ['babel']).on('change', reload)
+})
