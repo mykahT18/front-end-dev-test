79c3f15a4ac032ee4f0ce98bfc154f01f939d704
Mykah <mykah.taylor@gmail.com>
2017-11-02 18:21:47 -0400

    Finished Javascript problem 1

--- js-check/phoneNumber.js
+++ js-check/phoneNumber.js
@@ -8,7 +8,21 @@
 // remove dots as in "321.321.4321"
 
 const phoneNumber = (pNum) => {
-	let number = pNum.replace(/[^+\d]+/g, "");
+	let pattern = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
+	let number = pNum.replace(/[^\d]+/g, "");
+	if(number.length > 11 || number.length < 10 ){
+		return null
+	}
+	if(number.length === 11){
+		if(number[0] != 1){
+			return null
+		}
+		let newNumber = number.substr(1)
+		return newNumber
+	}
+	if(pattern.test(number) == false){
+		return null
+	}
 	return number
 }
 
@@ -17,3 +31,4 @@ module.exports = phoneNumber
 // function. Feel free to make as many functions as you see
 // fit. Just be sure to leave phoneNumber as an exported
 // method as you found it
+
